c1acaa8aaa92769c0397439592ada1a5e988f750
NEELAKANTA2000 <56578433+NEELAKANTA2000@users.noreply.github.com>
2020-04-20 23:22:56 +0530

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,42 @@
+from googletrans import Translator
+
+file_name=input("enter input file...")
+trans_lang=input("enter translation language")
+dest=trans_lang.capitalize()
+print(dest)
+file_name=open(file_name,'r')
+data=file_name.read()
+
+languages={
+    'Telugu' : 'te',
+    'Japanese' : 'ja',
+    'Russian' : 'ru',
+    'Italian' : 'it',
+    'Latin' : 'la',
+    'Hindi' : 'hi',
+    'Arabic' : 'ar',
+    'Greek' : 'el',
+    'Korean' : 'ko',
+    'Urdu' : 'ur',
+    'Gujarati' : 'gu',
+    'French':'fr',
+    'Bulgarian' : 'bg',
+    'Chinese' : 'zh-CN',
+    'Irish' : 'ga',
+    'German' : 'de',
+    'Indonesian' : 'id',
+    'Lao' : 'lo',
+    'Marathi' : 'mr',
+    'Persian' : 'fa',
+    'Sindhi': 'sd',
+    'Spanish' : 'es',
+    'Zulu':'zu'
+
+}
+output_lang=languages[dest]
+translator=Translator()
+
+
+
+print(translator.translate(data,dest=output_lang).text)
+#print("Input data is in ",translator.detect(data),"...")
